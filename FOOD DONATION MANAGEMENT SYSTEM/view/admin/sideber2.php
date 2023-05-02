ea7249a7315c789f20935a135fa1abacbda117e4
MD SUMON <67429465+sumu9897@users.noreply.github.com>
2023-05-02 19:58:42 +0600

    Add files via upload

---
+++ FOOD DONATION MANAGEMENT SYSTEM/view/admin/sideber2.php
@@ -0,0 +1,10 @@
+<fieldset>
+    <legend><h2>ADMIN</h2></legend>
+    <ul>
+        <li><a href="./profile.php">Profile</a></li>
+        <li><a href="./updateprofile.php">Update Profile</a></li>
+        <li><a href="./changepassword.php">Change Password</a></li>
+        <li><a href="./dashboard.php">Dashboard</a></li>
+        <LI><A href="../../controller/admin/logout_action.php">Log Out</A></LI>
+    </ul>
+</fieldset>
